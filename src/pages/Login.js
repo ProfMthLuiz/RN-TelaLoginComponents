7fb6ecfb5ee2031900bd0ddfae9a76ada63e958c
ProfMthLuiz <mthluiz99@gmail.com>
2024-03-08 21:47:29 -0300

    update telaLoginComponents

---
+++ src/pages/Login.js
@@ -0,0 +1,19 @@
+import ImgBackgroundComponent from "../components/ImgBackgroundComponent";
+import ImageComponent from "../components/ImageComponent";
+import TextComponent from "../components/TextComponent";
+import InputComponent from "../components/InputComponent";
+import ButtonComponent from "../components/ButtonComponent";
+import CardComponent from "../components/CardComponent";
+
+export default function Login() {
+  return (
+    <ImgBackgroundComponent>
+      <CardComponent>
+        <ImageComponent />
+        <TextComponent />
+        <InputComponent />
+        <ButtonComponent />
+      </CardComponent>
+    </ImgBackgroundComponent>
+  );
+}
